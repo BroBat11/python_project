4eaa7cf1d98665d2f312b07f59eccdf2dacff608
BroBat11 <127123524+BroBat11@users.noreply.github.com>
2023-12-25 22:47:19 +0600

    Delete DjangoAppp-master directory

--- DjangoAppp-master/biblo/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class BibloConfig(AppConfig):
-    # Use a large auto field for model identifiers.
-    default_auto_field = 'django.db.models.BigAutoField'
-    # Set the name of the application to "biblo".
-    name = 'biblo'
-    # Human-readable name for display in the Django admin panel.
-    verbose_name = 'Book'
